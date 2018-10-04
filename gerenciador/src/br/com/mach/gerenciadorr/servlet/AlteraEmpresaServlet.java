ef3c662cd25ce9ec6ac4c317a27cbcdec6839119
tls <tls@Machado-PC>
2018-10-04 15:02:27 -0300

    upData Aula_07 Completando o CRUD _fimDessa Aula_07

--- gerenciador/src/br/com/mach/gerenciadorr/servlet/AlteraEmpresaServlet.java
+++ gerenciador/src/br/com/mach/gerenciadorr/servlet/AlteraEmpresaServlet.java
@@ -35,7 +35,12 @@ public class AlteraEmpresaServlet extends HttpServlet {
 		}
 		
 		System.out.println(id);
-
+		
+		Banco banco = new Banco();
+		Empresa empresa = banco.buscaEmpresaPeloId(id);
+		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
+		response.sendRedirect("listaEmpresas");
 	}
 
 }
